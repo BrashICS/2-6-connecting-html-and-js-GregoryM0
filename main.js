bb40d5301bac0c7d62592e0ebbf816a83525e0fd
Gregory Murphy <gregory.murphy1@stu.ocsb.ca>
2024-10-17 10:42:21 -0400

    doc

--- main.js
+++ main.js
@@ -9,6 +9,13 @@
 'use strict';
 
 /*** Event Listeners ***/
+document.getElementById("d6").addEventListener("click", roll_d6)
+document.getElementById("d8").addEventListener("click", roll_d8)
+document.getElementById("d6").addEventListener("mouseenter", d6_enter)
+document.getElementById("d6").addEventListener("mouseleave", d6_leave)
+document.getElementById("d8").addEventListener("mouseenter", d8_enter)
+document.getElementById("d8").addEventListener("mouseleave", d8_leave)
+document.getElementById("enter").addEventListener("click", greeting)
 
 
 /*** Functions ***/
@@ -18,3 +25,33 @@ function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function roll_d6() {
+    let roll = randInt(1, 6);
+    document.getElementById("die_roll").textContent = roll;
+}
+
+function roll_d8() {
+    let roll = randInt(1, 8);
+    document.getElementById("die_roll").textContent = roll;
+}
+
+function d6_enter() {
+    document.getElementById("die").textContent = "🤡👹"
+}
+
+function d6_leave() {
+    document.getElementById("die").textContent = "👺"
+}
+
+function d8_enter() {
+    document.getElementById("die").textContent = "🌝"
+}
+
+function d8_leave() {
+    document.getElementById("die").textContent = "🫥"
+}
+
+function greeting() {
+    let name = document.getElementById("user.input").value;
+    document.getElementsById("greeting").textcontent = `Hello ${name}`
+}
